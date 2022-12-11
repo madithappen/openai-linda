cc2ca8d5c2820c3b6887a6b02423e66cefb35ad6
Stephen Abbott <stephenabbott20@gmail.com>
2022-12-11 06:31:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ai-linda
+
+### This program interacts with OpenAI's ChatGPT model to return top-tier responses to the intput provided.
+#### The program accepts STDIN, or input from microphone
+#### The program outputs STDOUT, or sounds to speakers
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ai_linda",
+    version="1.0.0",
+    description="A project leveraging OpenAI to interact with OpenAI",
+    author="Stephen M Abbott",
+    author_email="stephenabbott20@gmail.com",
+    license="MIT",
+    packages=[],
+    install_requires=[],
+    zip_safe=False,
+)
